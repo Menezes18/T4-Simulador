296b80f57fcfff2004438ae529a78a4d9509a87b
Menezes18 <gustavo.e.menezes@outlook.com>
2023-08-28 15:40:42 -0300

    Energia e fome - PlayerStatus, UIManager

---
+++ Assets/Scripts/M_SystemTree/TreeData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TreeData / Tree")]
+public class TreeData : ScriptableObject
+{
+    public string name;
+    public int vida;
+    public GameObject drop;
+    
+}
